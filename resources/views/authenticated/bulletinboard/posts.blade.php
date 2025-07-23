5d44edc55af2838f7a74c4326aceabc59b1907ac
ManaAsai <niconicochan1012@gmail.com>
2025-07-23 22:19:05 +0900

    コメントのカウント表示

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -12,25 +12,33 @@
                     <div class="post_bottom_area d-flex">
                         <div class="d-flex post_status">
                             <div class="mr-5">
+
+                                {{-- コメント数の表示 --}}
                                 <i class="fa fa-comment"></i>
-                                <span class="commentUser{{}}"></span>
+                                <span class="comment_counts{{ $post->id }}">
+                                    {{ $post->postComments->count() }}
+                                </span>
+                                {{--  $post->postComments⇒投稿のコメント一覧 -リレーションから引っ張ってきている-}}
                             </div>
-                            <div>
-                                @if (Auth::user()->is_Like($post->id))
-                                    <p class="m-0"><i class="fas fa-heart un_like_btn"
-                                            post_id="{{ $post->id }}"></i><span
-                                            class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span>
-                                    </p>
-                                @else
-                                    <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i>
-                                        {{-- <span class="like_counts{{ $post->id }}"></span></p> --}}
-                                        <span
-                                            class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span>
-                                @endif
+
+                            {{-- いいねの数の表示 --}}
+                                <div>
+                                    @if (Auth::user()->is_Like($post->id))
+                                        <p class="m-0"><i class="fas fa-heart un_like_btn"
+                                                post_id="{{ $post->id }}"></i><span
+                                                class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span>
+                                        </p>
+                                    @else
+                                        <p class="m-0"><i class="fas fa-heart like_btn"
+                                                post_id="{{ $post->id }}"></i>
+                                            {{-- <span class="like_counts{{ $post->id }}"></span></p> --}}
+                                            <span
+                                                class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span>
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
             @endforeach
         </div>
         <div class="other_area border w-25">
